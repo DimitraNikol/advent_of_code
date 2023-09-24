e41b012b6fda3cdc9b5e63bc3d39aa7e0a8754b0
Dimitra Nikolaidi <137819638+DimitraNikol@users.noreply.github.com>
2023-09-24 16:54:13 +0300

    part2

--- 2021.py
+++ 2021.py
@@ -1,6 +1,7 @@
 with open('input.txt', 'r') as d:
     lines = d.readlines()
 
+
 def count_increased(lines):
     increased = 0
 
@@ -9,7 +10,20 @@ def count_increased(lines):
             increased += 1
     return increased
 
-print(count_increased(lines))
+
+
+def sums_of_three(lines):
+    sumo_list = []
+    for i in range(0,len(lines) -2):
+        sumo_three = int(lines[i]) + int(lines[i+1]) + int(lines[i+2])
+        sumo_list.append(sumo_three)
+    return sumo_list
+
+print(count_increased(sums_of_three(lines)))
+
+
+
+
 
 
 
